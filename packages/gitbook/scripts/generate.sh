8fcc29b0c88e1562b49b7a37d3663d447541d7ff
corylam <cory.lam@orbitax.com>
2025-12-16 16:15:00 -0800

    Fix Windows bash scripts and generation

--- packages/gitbook/scripts/generate.sh
+++ packages/gitbook/scripts/generate.sh
@@ -6,7 +6,7 @@ set -o pipefail
 # Copy the assets
 gitbook-icons ./public/~gitbook/static/icons custom-icons
 gitbook-math ./public/~gitbook/static/math
-cp -r ../embed/standalone/ ./public/~gitbook/static/embed
+cp -R ../embed/standalone/ ./public/~gitbook/static/embed
 
 # Generate the types
 wrangler types
